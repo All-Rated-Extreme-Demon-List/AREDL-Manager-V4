57474c316884dc365a9dd8f0da3b33300f32f74d
sphericle <jackb2033@gmail.com>
2026-01-28 22:10:52 -0500

    points commands

---
+++ src/app/commands/list/points.tsx
@@ -0,0 +1,19 @@
+import { ChatInputCommand, CommandData } from "commandkit";
+
+import { SlashCommandBuilder, ChatInputCommandInteraction, MessageFlags } from 'discord.js';
+import { enableStaffPoints, defaultPoints } from '@/../config.json';
+import { StaffPoints } from "@/db/models"
+
+export const command: CommandData = {
+    name: "points",
+    description: "View your total Pukeko Points"
+}
+
+export const chatInput: ChatInputCommand = async ({interaction}) => {
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        const [user, _] = await StaffPoints.findOrCreate({ 
+            where: { user: interaction.user.id },
+            defaults: { points: defaultPoints },
+        });
+        return await interaction.editReply(`You have **${Math.round(user.dataValues.points * 100) / 100}** Pukeko Points.`);
+}
---
+++ src/app/commands/list/pointsAdmin.tsx
@@ -0,0 +1,211 @@
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  ContainerBuilder,
+  SeparatorSpacingSize,
+  TextDisplayBuilder,
+  MessageFlags,
+  ApplicationCommandOptionType,
+} from "discord.js";
+import { enableStaffPoints, defaultPoints, maxPoints } from "@/../config.json";
+import {
+  CommandData,
+  ChatInputCommand,
+  Container,
+  TextDisplay,
+  Separator,
+} from "commandkit";
+import { StaffPoints } from "@/db/models";
+
+export const command: CommandData = {
+  name: "pointsadmin",
+  description: "Commands for admins to manage the staff points system",
+  options: [
+    {
+      name: "all",
+      description: "View all staff members' points values",
+      type: ApplicationCommandOptionType.Subcommand,
+    },
+    {
+      name: "set",
+      description: "Set the point values of a staff member",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "user",
+          description: "The user to set the points value for",
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+        {
+          name: "points",
+          description: "The points value to set",
+          type: ApplicationCommandOptionType.Number,
+          required: true,
+          min_value: 0,
+          max_value: 30,
+        },
+      ],
+    },
+    {
+      name: "transfer",
+      description: "Transfer a staff member's points a different staff member",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "transfer-from",
+          description: "The user whose points will be transfered",
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+        {
+          name: "transfer-to",
+          description: "The user to transfer the points to",
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+        {
+          name: "overwrite",
+          description:
+            "Whether to overwrite the recipient's points instead of adding to them",
+          type: ApplicationCommandOptionType.Integer,
+          required: true,
+          choices: [
+            { name: "No", value: 0 },
+            { name: "Yes", value: 1 },
+          ],
+        },
+      ],
+    },
+    {
+      name: "clear",
+      description: "Clear a staff member's points",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "user",
+          description: "The user to clear the points for",
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "find",
+      description: "Find a staff member's points",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "user",
+          description: "The user to find the points for",
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+      ],
+    },
+  ],
+};
+
+export const chatInput: ChatInputCommand = async ({ interaction }) => {
+  const subcommand = interaction.options.getSubcommand();
+  await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+  if (subcommand === "all") {
+    const allStats = await StaffPoints.findAll({
+      order: [["points", "DESC"]],
+    });
+
+    const container = (
+      <Container accentColor={0x75c8ff}>
+        <TextDisplay>## Staff Points</TextDisplay>
+        <Separator spacing={SeparatorSpacingSize.Small} />
+        <TextDisplay>
+          {allStats.length == 0
+            ? "No data."
+            : allStats
+                .map(
+                  (stat) =>
+                    `<@${stat.dataValues.user}> - _\`${Math.round(stat.dataValues.points * 100) / 100}\` points_`,
+                )
+                .join("\n")}
+        </TextDisplay>
+      </Container>
+    );
+
+    return await interaction.editReply({
+      components: [container],
+      flags: MessageFlags.IsComponentsV2,
+    });
+  } else if (subcommand === "find") {
+    const user = interaction.options.getUser("user", true);
+    const [points, _] = await StaffPoints.findOrCreate({
+      where: { user: user.id },
+      defaults: { points: defaultPoints },
+    });
+    return await interaction.editReply(
+      `<@${user.id}> has ${Math.round(points.dataValues.points * 100) / 100} points.`,
+    );
+  } else if (subcommand === "set") {
+    const user = interaction.options.getUser("user", true);
+    const points = interaction.options.getNumber("points", true);
+    const [entry, created] = await StaffPoints.findOrCreate({
+      where: {
+        user: user.id,
+      },
+      defaults: {
+        points: points,
+      },
+    });
+
+    if (!created) {
+      entry.dataValues.points = points;
+      entry.save();
+    }
+    return await interaction.editReply(
+      `:white_check_mark: Set points for <@${user.id}> to ${Math.round(points * 100) / 100}.`,
+    );
+  } else if (subcommand === "transfer") {
+    const transferFrom = interaction.options.getUser("transfer-from", true);
+    const transferTo = interaction.options.getUser("transfer-to", true);
+    const overwrite = interaction.options.getInteger("overwrite") === 1;
+    const transferFromPoints = await StaffPoints.findOne({
+      where: { user: transferFrom.id },
+    });
+    if (!transferFromPoints) {
+      return await interaction.editReply(
+        `:x: <@${transferFrom.id}> does not have any points.`,
+      );
+    }
+    const [transferToPoints, _] = await StaffPoints.findOrCreate({
+      where: { user: transferTo.id },
+      defaults: { points: 0 },
+    });
+
+    transferToPoints.dataValues.points = overwrite
+      ? transferFromPoints.dataValues.points
+      : Math.min(
+          transferToPoints.dataValues.points +
+            transferFromPoints.dataValues.points,
+          maxPoints,
+        );
+
+    transferToPoints.save();
+    transferFromPoints.destroy();
+    return await interaction.editReply(
+      `:white_check_mark: Transferred points from <@${transferFrom.id}> to <@${transferTo.id}>. <@${transferTo.id}> now has ${Math.round(transferToPoints.dataValues.points * 100) / 100} points.`,
+    );
+  } else if (subcommand === "clear") {
+    const user = interaction.options.getUser("user", true);
+    const points = await StaffPoints.findOne({
+      where: { user: user.id },
+    });
+    if (!points) {
+      return await interaction.editReply(
+        `:x: <@${user.id}> does not have any points.`,
+      );
+    }
+    points.destroy();
+    return await interaction.editReply(
+      `:white_check_mark: Cleared points for <@${user.id}>.`,
+    );
+  }
+};
