312eb786bef29630be8ce763275c24ad85b576b5
sphericle <jackb2033@gmail.com>
2026-01-31 13:11:43 -0500

    --__--

--- src/util/commandGuilds.ts
+++ src/util/commandGuilds.ts
@@ -3,6 +3,6 @@ import { CommandMetadataFunction } from "commandkit"
 import { guildId, staffGuildId } from "@/../config.json";
 export const commandGuilds: CommandMetadataFunction = () => {
 	return {
-		guilds: [guildId, staffGuildId],
+		guilds: Array.from(new Set([guildId, staffGuildId])),
 	}
 }
